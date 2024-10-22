e882f775b6f863dcae11da7f7d1584f642b7149a
Max Ortner <maxortner01@gmail.com>
2024-10-21 18:33:38 -0600

    made into library

--- res/shaders/main.lua
+++ res/shaders/main.lua
@@ -7,10 +7,6 @@ Polygon = {
 }
 
 Pipeline = {
-    -- shaders = {
-       -- vertex   = "/shaders/vertex.glsl",
-      --  fragment = "/shaders/fragment.glsl"
-    --},
     formats = { 37, 97, 97 },
     colorFormat = VK_FORMAT_B8G8R8A8_UNORM,
     depthFormat = VK_FORMAT_D32_SFLOAT_S8_UINT,
